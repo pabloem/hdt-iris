4115ff79b4bb5eaa95e9a5323f4bac8042789bdb
pabloem <pabloem@ucla.edu>
2015-07-18 11:57:21 +0200

    levelstreamer interactive

--- src/main/java/HdtConsolidator.java
+++ src/main/java/HdtConsolidator.java
@@ -5,23 +5,53 @@ import java.util.*;
 
 public class HdtConsolidator {
   public static final String rdf2hdt = "./rdf2hdt";
-  public static final String levelStreamer = "../../../levelStreamer.js";
+  public static final String levelStreamer = "../../../levelStreamerLine.js";
+  public static final String hdtSearch = "./hdtSearch";
+
+  private String added_db;
+  private String removed_db;
+  private String hdt_in;
+  private String hdt_out;  
+
   public HdtConsolidator(String added, String removed, 
                          String hdt, String output) {
-    String cmdAdded = levelStreamer + " " + added;
+    added_db = added;
+    removed_db = removed;
+    hdt_in = hdt;
+    hdt_out = output;
+  }
+
+  public void run() {
+    String cmdRemoved = levelStreamer + " " + removed_db;
+    appendAdded();
+  }
+
+  private void appendAdded() {
+    String cmdAdded = levelStreamer + " " + added_db;
+    System.out.println("Appending added...");
     try {
       Process p = Runtime.getRuntime().exec(cmdAdded);
-
-      BufferedReader addedInp = new BufferedReader( new InputStreamReader(p.getInputStream()) );
-      BufferedWriter addedOut = new BufferedWriter( new OutputStreamWriter(p.getOutputStream()) );
-      System.out.println("Hi testing");
-
+      BufferedReader addedOut = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      BufferedWriter addedInp = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+      addedInp.write("\n");
+      addedInp.flush();
+      addedInp.write("\n");
+      addedInp.flush();
+      addedInp.write("\n");
+      addedInp.flush();
       String line = "";			
-      while ((line = addedInp.readLine())!= null) {
-        System.out.println(line);
+      while ((line = addedOut.readLine())!= null) {
+        appendToHdt(line);
       }
+      addedOut.close();
+      addedInp.close();
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  private void appendToHdt(String triple) {
+    // TODO - implement. Add line to HDT file.
+    System.out.println(triple);
   }
 };
