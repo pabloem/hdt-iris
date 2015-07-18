219a761c0eabe67648bc68713a7454892f4d87a2
pabloem <pabloem@ucla.edu>
2015-07-18 09:37:41 +0200

    level database

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class test {
-  public statis void main(String[] args) {
-    HdtConsolidator hc = new HdtConsolidator('workspace/added.db/');
+  public static void main(String[] args) {
+    HdtConsolidator hc = new HdtConsolidator("../../../workspace/added.db/","","","");
   }
 }
