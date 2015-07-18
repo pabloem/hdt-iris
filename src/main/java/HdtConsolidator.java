d1c81c6019d735b687930761e57297401d8ab83e
pabloem <pabloem@ucla.edu>
2015-07-19 00:11:33 +0200

    hihihi

--- src/main/java/HdtConsolidator.java
+++ src/main/java/HdtConsolidator.java
@@ -23,31 +23,37 @@ public class HdtConsolidator {
 
   public void run() {
     String cmdRemoved = levelStreamer + " " + removed_db;
+    addSourceHdt();
     appendAdded();
   }
 
-  private void appendAdded() {
-    String cmdAdded = levelStreamer + " " + added_db;
+  private void addSourceHdt() {
+    String cmdHdt = hdtSearch+ " "+hdt_in;
+    LevelGraphStreamer rmvDb = new LevelGraphStreamer(removed_db);
+    rmvDb.init();
+    rmvDb.preCache();
     try {
-      Process p = Runtime.getRuntime().exec(cmdAdded);
+      Process p = Runtime.getRuntime().exec(cmdHdt);
       BufferedReader addedOut = new BufferedReader(new InputStreamReader(p.getInputStream()));
-      BufferedWriter addedInp = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
-      addedInp.write("\n");
-      addedInp.flush();
-      addedInp.write("\n");
-      addedInp.flush();
-      addedInp.write("\n");
-      addedInp.flush();
       String line = "";			
-      while ((line = addedOut.readLine())!= null) {
-        if(line.length() < 5) break;
+      while ((line = lgAdd.getTriple())!= null) {
+        if(rmvDb.contains(line)) continue;
         appendToHdt(line);
       }
-      addedOut.close();
-      addedInp.close();
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  private void appendAdded() {
+    LevelGraphStreamer lgAdd = new LevelGraphStreamer(added_db);
+    lgAdd.init();
+    lgAdd.startQuery("","","");
+    String line = "";			
+    while ((line = lgAdd.getTriple())!= null) {
+      appendToHdt(line);
+    }
+    lgAdd.finalize();
   }
 
   private void appendToHdt(String triple) {
---
+++ src/main/java/LevelGraphStreamer.java
@@ -0,0 +1,57 @@
+class LevelGraphStreamer {
+  public static final String levelStreamer = "../../../levelStreamerLine.js";
+  private String streamFile;
+  private BufferedReader stdOut;
+  private BufferedWriter stdIn;
+  private Process pr;
+  public LevelGraphStreamer(file) {
+    streamFile = file;
+  }
+  public init() {
+    String cmd = levelStreamer + " " + streamFile;
+    pr = Runtime.getRuntime().exec(cmd);
+    stdOut = new BufferedReader(new InputStreamReader(p.getInputStream()));
+    stdIn = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+  }
+  public preCache() {
+    // Pre-caches the contents of a levelgraph database to a Bloom filter
+    // TODO implement
+  }
+  public finalize() {
+    stdOut.close();
+    stdIn.close();
+  }
+  public boolean contains(String triple) {
+    Pattern pattern = Pattern.compile("^(?:<([^>]+)>\s*){2}<?([^>]+)>?$");
+    Matcher matcher = pattern.matcher(triple);
+    if(matcher.matches()) {
+      System.out.print(matcher.group(1)+" "+matcher.group(2)+" "+matcher.group(3));
+      startQuery(matcher.group(1),matcher.group(2),matcher.group(3));
+    } else {
+      /* Gotta error out */
+    }
+    if(getTriple() == null) return false;
+    getTriple() // Get the last line
+    return true;
+  }
+  public String getTriple() {
+    String line = stdOut.readLine();
+    if(line.length() < 5) {
+      // A string of less than 5 characters can not be a triple.
+      return null;
+    }
+    return line;
+  }
+  public void startQuery(String subject,String predicate,String object) {
+    try{ 
+      stdIn.write(subject+"\n");
+      stdIn.flush();
+      stdIn.write(predicate+"\n");
+      stdIn.flush();
+      stdIn.write(object+"\n");
+      stdIn.flush();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+};
