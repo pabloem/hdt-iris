363e0766377a74b596f5b2900009409ded9e245d
pabloem <pabloem@ucla.edu>
2015-07-22 11:44:43 +0200

    another

--- src/main/java/HdtConsolidator.java
+++ src/main/java/HdtConsolidator.java
@@ -4,14 +4,15 @@ import java.io.*;
 import java.util.*;
 
 public class HdtConsolidator {
-  public static final String rdf2hdt = "./rdf2hdt";
+  public static final String rdf2hdt = "../../../rdf2hdt";
   public static final String levelStreamer = "../../../levelStreamerLine.js";
-  public static final String hdtSearch = "./hdtSearch";
+  public static final String hdtSearch = "../../../hdtSearch";
 
-  private String added_db;
-  private String removed_db;
-  private String hdt_in;
-  private String hdt_out;  
+  private String added_db;       // Our levelgraph with added elements
+  private String removed_db;     // Our levelgraph with removed elements
+  private String hdt_in;         // The HDT input file
+  private String hdt_out;        // The resulting HDT file
+  private BufferedWriter fifo;   // Our intermediate FIFO
 
   public HdtConsolidator(String added, String removed, 
                          String hdt, String output) {
@@ -21,10 +22,43 @@ public class HdtConsolidator {
     hdt_out = output;
   }
 
-  public void run() {
+  private String generateFifoName(int len) {
+    String AB = "0123456789abcdefghijklmnopqrstuvwxyz";
+    Random rnd = new Random();
+    StringBuilder sb = new StringBuilder( len );
+    for( int i = 0; i < len; i++ ) 
+      sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
+    return sb.toString();
+  }
+
+  public void setupRun() throws Exception {
+    /* First, we create out 'named pipe', or fifo 
+    String fifoFile = "/tmp/"+generateFifoName(6);
+    String mkFifoCmd = "mkfifo "+fifoFile;
+    Process p Runtime.getRuntime().exec(mkFifoCmd);
+    int res = p.waitFor();*/
+
+    /* Second, we get a write stream to our named pipe 
+       fifo = new BufferedWriter(new BufferedOutputStream(new FileOutputStream(fifoFile)));*/
+    
+    /* Third, we start executing the rdf2hdt process 
+    String rdf2hdtCmd = rdf2hdt + " " + fifoFile + " " + hdt_out;
+    Process p Runtime.getRuntime().exec(rdf2hdtCmd);*/
+
+    return ;
+  }
+
+  public void finalize() {
+    /*fifo.newLine();
+      fifo.close();*/
+  }
+
+  public void run() throws Exception {
     String cmdRemoved = levelStreamer + " " + removed_db;
+    setupRun();
     addSourceHdt();
     appendAdded();
+    finalize();
   }
 
   private void addSourceHdt() {
